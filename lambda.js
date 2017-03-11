d9a8ae5761727d8f8ab019484a0d781fe11dfa62
Supanut Apikulvanich <supanut.overload@gmail.com>
2017-03-11 11:46:38 +0700

    Updated lambda.js

--- lambda.js
+++ lambda.js
@@ -1,3 +1,8 @@
-exports.webhook = (event, context, callback) => (
-  callback(null, 'Hello, ' + event.name || '[NO_NAME]')
-)
+exports.webhook = (event, context, callback) => {
+  const body = JSON.parse(event.body || null)
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ "message": `Hello, ${body.name}` })
+  }
+  callback(null, response)
+}
