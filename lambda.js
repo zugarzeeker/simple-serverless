204058f313570229fae60b6d3137606ba5eca640
Supanut Apikulvanich <Supanut.Overload@gmail.com>
2017-03-11 11:47:42 +0700

    Formatted code

--- lambda.js
+++ lambda.js
@@ -2,7 +2,7 @@ exports.webhook = (event, context, callback) => {
   const body = JSON.parse(event.body || null)
   const response = {
     statusCode: 200,
-    body: JSON.stringify({ "message": `Hello, ${body.name}` })
+    body: JSON.stringify({ message: `Hello, ${body.name}` })
   }
   callback(null, response)
 }
